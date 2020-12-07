d20407678f076a85e3849f24d66a97350b6f6578
xioni3 <42072580+xioni3@users.noreply.github.com>
2020-12-07 20:15:02 +0100

    Added Pandas json analysis

--- json_annotations.py
+++ json_annotations.py
@@ -1,27 +1,35 @@
 import json
 import matplotlib.pyplot as plt
+import pandas as pd
 
-def count():
-    json_path = 'K:/dataset/flir dataset/video/thermal_annotations.json'
+def count_people():
+    json_path = 'K:/dataset/flir dataset/train/thermal_annotations.json'
+    output_path = 'K:/dataset/flir output dataset/'
+    category_dict_path = 'K:/dataset/flir dataset/train/thermal_annotations.json'
     with open(json_path) as json_file:
         data = json.load(json_file)
-        annotations_dict = dict()
-        for i in data['images']:
-            if not i['id'] in annotations_dict:
-                annotations_dict[i['id']] = 0
-        for a in data ['annotations']:
-            if len(a['bbox'])>0 and a['category_id']==1:
-                annotations_dict[a['image_id']] +=1
-    distribution_dict = dict()
-    for i in annotations_dict.values():
-        if not i in distribution_dict:
-            distribution_dict[i] = 0
-        distribution_dict[i]+=1
-    plt.bar(distribution_dict.keys(), distribution_dict.values())
-    plt.title('FLIR dataset person count statistics')
-    plt.ylabel('images')
-    plt.xlabel('annotated people')
+        categories = ['person','car','bicycle','dog']
+        cat = pd.DataFrame(data['categories']).rename(columns={'id':'category_id','name':'category'})
+        #for c in categories:
+        df = pd.merge(pd.DataFrame(data['annotations']),cat,on=['category_id'],how='inner')
+        df.to_excel('annotations.xlsx')
+        #gby_category = df.value_counts(['category'])
+        #plt.bar(range(len(gby_category)), gby_category.values, align='center')
+        #plt.xticks(range(len(gby_category)), gby_category.index.values, size='small')
+        #plt.show()
+        #people_group = df[['image_id','category']].groupby(['image_id','category']).count().reset_index(name='count')
+        #people_group.to_excel("people_group.xlsx")
+        #gby_ann_num = people_group.groupby(['category','count']).size().reset_index(name='count_q')
+        #plt.bar(gby_ann_num['count_q'], gby_ann_num.index.values, align='center')
+        #plt.show()
+        #gby_people.hist(by)
+        #print(people_group)
 
-    plt.show()
+
+def count_all():
+    json_path = 'K:/dataset/flir dataset/video/thermal_annotations.json'
+
+
 if __name__ == '__main__':
-    count()
+    count_people()
+    #count_all()
